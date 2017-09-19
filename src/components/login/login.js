722f731cdac3e4e7c7f3b2cd9f0865550123e659
Ahmed Raza Qadri <razatechuc11@gmail.com>
2017-09-19 23:53:27 +0500

    First Page of UI Kitten Completed

---
+++ src/components/login/login.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    TextInput,
+    Button,
+    TouchableOpacity
+} from 'react-native';
+import { loginStyle } from './loginStyle';
+class Login extends Component {
+    render() {
+        return (
+            <View>
+                <View style={loginStyle.container}>
+                    <View style={loginStyle.iconBox}>
+                        <Image source={require('../../images/icon.png')} style={loginStyle.imageIcon} />
+                        <Text style={loginStyle.iconText1}>React Native</Text>
+                        <Text style={loginStyle.iconText2}>UI Kitten</Text>
+                    </View>
+                    <View style={loginStyle.inputBox}>
+                        <View>
+                            <TextInput placeholder="Username" underlineColorAndroid="transparent" style={loginStyle.inputStyle} />
+                            <TextInput placeholder="Password" underlineColorAndroid="transparent" style={loginStyle.inputStyle} />
+                            <TouchableOpacity style={loginStyle.loginButton}>
+                                <Text style={loginStyle.loginButtonText}>LOGIN</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={loginStyle.socialIconContainer}>
+                            <TouchableOpacity >
+                                <Image
+                                    style={loginStyle.socialIcon}
+                                    source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS73_kc4hKnABULErHXnUK2Pcty3qVhOZUX6vmlQz9M2tWZ8C7xWA' }}
+                                />
+                            </TouchableOpacity>
+                            <TouchableOpacity >
+                                <Image
+                                    style={loginStyle.socialIconGmail}
+                                    source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAn1yX5SKPQK91Vddb03gN8poRKKaea-7hR6ZdkQmwa2frgSrH' }}
+                                />
+                            </TouchableOpacity>
+                            <TouchableOpacity >
+                                <Image
+                                    style={loginStyle.socialIcon}
+                                    source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb8Ed1pBiZppBpe-EaFPCkXeTga6CW367n3Dld89IeznfVH-kyeEOvteA' }}
+                                />
+                            </TouchableOpacity>
+
+                        </View>
+                        <View style={loginStyle.accountCreateSuggest}>
+                            <Text>Don't have an account?</Text><Text style={{fontWeight:'bold'}}> Sign up now</Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        );
+    }
+}
+
+export default Login;
---
+++ src/components/login/loginStyle.js
@@ -0,0 +1,88 @@
+export const loginStyle = {
+    iconBox: {
+        alignItem: 'center',
+        marginTop: 30,
+        alignItem: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center'
+        // borderWidth:1,
+        // borderColor:'black'
+    },
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItem: 'center',
+        justifyContent: 'center'
+    },
+    iconText1: {
+        fontSize: 27,
+        color: 'black',
+        fontWeight: 'lighter',
+        textAlign: 'center'
+
+    },
+    iconText2: {
+        fontSize: 33,
+        color: 'black',
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    imageIcon: {
+        width: 120,
+        height: 110,
+        borderWidth: 1,
+        marginLeft: "auto",
+        marginRight: "auto",
+
+    },
+    inputBox: {
+        display: 'flex',
+        flexDirection: 'column',
+        padding: 10,
+    },
+    inputStyle: {
+        borderWidth: 1,
+        height:50,
+        borderColor: '#D3D3D3',
+        borderRadius: 30,
+        marginTop: 12,
+        padding:10
+    },
+    loginButton: {
+        marginTop: 23,
+        borderRadius: 30,
+        backgroundColor: 'orange',
+        padding: 12,
+        backgroundImage:"url(../../images/loginButtonBackground.png)"
+    },
+    loginButtonText: {
+        textAlign: 'center',
+        fontSize: 18,
+        color: 'white'
+    },
+    socialIconContainer: {
+        marginTop:15,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    },
+    socialIcon: {
+        width: 50,
+        height: 50,
+        borderRadius: 30,
+        borderWidth:1,
+        borderColor:'#D3D3D3'
+    },
+    socialIconGmail: {
+        width: 50,
+        height: 50,
+        borderRadius: 30,
+        borderWidth:1,
+        borderColor:'#D3D3D3'
+    },
+    accountCreateSuggest:{
+        flexDirection:'row',
+        justifyContent:'center',
+        marginTop:15
+    }
+}
